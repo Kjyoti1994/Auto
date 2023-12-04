009a2666acce7c4c6f0cd1bd3b9b1fdcf69579e7
VINOD <VINOD@OFFICE>
2023-12-04 14:07:45 +0530

    [Jyoti]:code push

---
+++ src/List/ArrayWithInt.java
@@ -0,0 +1,11 @@
+package List;
+
+import java.util.ArrayList;
+
+public class ArrayWithInt 
+{
+public static void main(String[] args) {
+	ArrayList<Integer> a =new ArrayList<Integer>();
+	a.add(101);
+}
+}
---
+++ src/List/Arraywithtype.java
@@ -0,0 +1,14 @@
+package List;
+
+import java.util.ArrayList;
+
+public class Arraywithtype
+{
+public static void main(String[] args) {
+	ArrayList<String> a=new ArrayList<String>();
+	a.add("Ram");
+	a.add("101");
+	
+	System.out.println(a);
+}
+}
